217331c0f4fb79d99925e97d7c4dd0df8a2beff2
Khoa Trinh <trinhleanhkhoa1999@gmail.com>
2024-01-03 16:42:11 +0700

    khoa: test setup router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import NotFound from "./pages/NotFound";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+
+const Layout = () => (
+  <div className="layout-app">
+    <Header />
+    <Outlet />
+    <Footer />
+  </div>
+);
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      errorElement: <NotFound />,
+      children: [{ index: true, element: <HomePage /> }],
+    },
+
+    {
+      path: "/login",
+      element: <LoginPage />,
+    },
+
+    {
+      path: "/register",
+      element: <RegisterPage />,
+    },
+  ]);
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <RouterProvider router={router} />
     </>
   );
 }
