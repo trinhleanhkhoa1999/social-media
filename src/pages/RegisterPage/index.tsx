9ccf9f1cbcb6e01c4ffb68970f8100de8d870d4c
Thao <nguyenbangocthao86@gmail.com>
2024-01-12 22:46:13 +0700

    Commit 3

--- src/pages/RegisterPage/index.tsx
+++ src/pages/RegisterPage/index.tsx
@@ -10,7 +10,7 @@ export default function RegisterPage() {
         alt="Your Company"
       />
       <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-        Sign in to your account
+        Sign up
       </h2>
     </div>
 
