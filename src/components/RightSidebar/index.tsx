9da07b84a7361bb83a0241c518d6b7c80183a13e
Khoa Trinh <trinhleanhkhoa1999@gmail.com>
2024-01-25 21:42:08 +0700

    khoa: demo homepage

--- src/components/RightSidebar/index.tsx
+++ src/components/RightSidebar/index.tsx
@@ -1,6 +1,8 @@
 import { BsSearch } from "react-icons/bs";
 
-const RightSidebar = () => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const RightSidebar = ({ listArticles }: any) => {
+  console.log(listArticles);
   return (
     <section className="text-white sticky top-0 mt-2 w-full flex flex-col items-stretch h-screen px-6 ml-auto">
       <div>
@@ -20,14 +22,26 @@ const RightSidebar = () => {
         </div>
       </div>
       <div className="flex flex-col rounded-xl bg-neutral-900 my-4">
-        <h3 className="font-bold text-2xl px-4 my-4">what's happening</h3>
+        <h3 className="font-bold text-2xl px-4 my-4">Popular Tags</h3>
         <div>
-          {Array.from({ length: 5 }).map((_, i) => (
+          {listArticles &&
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            listArticles.map((item: any, idx: any) => {
+              return (
+                <div
+                  key={idx}
+                  className="hover:bg-white/10 p-4 last:rounded-b-xl"
+                >
+                  <div className="font-bold text-lg">{item.tagList[0]}</div>
+                </div>
+              );
+            })}
+          {/* {Array.from({ length: 5 }).map((_, i) => (
             <div key={i} className="hover:bg-white/10 p-4 last:rounded-b-xl">
               <div className="font-bold text-lg">#trending ${i + 1}</div>
               <div className="text-xs text-neutral-400">35.2k</div>
             </div>
-          ))}
+          ))} */}
         </div>
       </div>
       <div></div>
