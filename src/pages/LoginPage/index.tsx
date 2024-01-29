d49d31228d670181a6578345e9334d753dafca43
Thao <nguyenbangocthao86@gmail.com>
2024-01-29 19:51:57 +0700

    Thao

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -56,11 +56,11 @@ export default function Login() {
             />
           </div>
         </div>
-
+        
         <div>
           <button
             type="submit"
-            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="flex mt-8 w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Sign in
           </button>
